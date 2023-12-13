7654e9a29504b384b1671675e6069f1d2deb6f34
Dym03 <hdymacek@gmail.com>
2023-12-13 17:41:06 +0100

    Day 13 - looked at the solution, didn't like this one >{ part 2 incoming, with my solution after christmas

--- Advent_2023/Day_13/main.cpp
+++ Advent_2023/Day_13/main.cpp
@@ -3,96 +3,65 @@
 #include <set>
 #include <vector>
 
-bool is_palindrome(std::string input, int &pos, int length, int &act_pos) {
-  if (input.length() < 1) {
-    return false;
-  }
-  int l = 0;
-  int r = input.length() - 1;
-  while (l < r) {
-    if (input[l] != input[r]) {
-      return false;
+// Source "https://www.youtube.com/watch?v=Jzp8INWz5Z0"
+int check_column_symetry(std::vector<std::string> pattern, int i) {
+  int rows = pattern.size();
+  int cols = pattern[0].length();
+  int baddness = 0;
+  for (int c = 0; c < cols; c++) {
+    for (int r = 0; r < rows; r++) {
+      int r2 = i * 2 + 1 - r;
+      if (r2 >= 0 && r2 < rows) {
+        if (pattern[r][c] != pattern[r2][c]) {
+          return false;
+        }
+      }
     }
-    l += 1;
-    r -= 1;
   }
-  pos = l;
-  act_pos = l + (length - input.length());
   return true;
 }
 
-bool left_side_equals_right_side(std::string input, int &pos, int length,
-                                 int &act_pos) {
-  if (is_palindrome(input, pos, length, act_pos)) {
-    return true;
-  } else {
-    return left_side_equals_right_side(input.substr(1), pos, length, act_pos) ||
-           left_side_equals_right_side(input.substr(0, input.length() - 1), pos,
-                                       length, act_pos);
-  }
-}
-
-bool up_side_equals_down_side(std::vector<std::string> patter, int &pos,
-                              int &act_pos, std::set<int> &positions) {
-  std::string tmp = "";
-  for (int i = 0; i < patter[0].size(); i++) {
-    for (int j = 0; j < patter.size(); j++) {
-      tmp += patter[j][i];
+std::vector<std::string> transpose(std::vector<std::string> &pattern) {
+  std::vector<std::string> copy;
+  for (int i = 0; i < pattern[0].size(); i++) {
+    copy.push_back("");
+    for (int j = 0; j < pattern.size(); j++) {
+      copy[i] += pattern[j][i];
     }
-    // std::cout << tmp;
-    if (left_side_equals_right_side(tmp, pos, tmp.length(), act_pos)) {
-      positions.insert(pos);
-      // std::cout << "True " << pos << '\n';
-    }
-    tmp.clear();
   }
-  return true;
+  return copy;
 }
 
 int main() {
   std::vector<std::string> pattern;
   std::string input_line;
   std::set<int> positions;
-  int pos = 0;
-  int act_pos = 0;
   int total_sum = 0;
   while (getline(std::cin, input_line)) {
     if (input_line == "") {
-      for (int i = 0; i < pattern.size(); i++) {
-        if (left_side_equals_right_side(pattern[i], pos, pattern[i].length(),
-                                        act_pos)) {
-          positions.insert(pos);
+      int h_pos = -1;
+      int v_pos = -1;
+      for (int i = 0; i < pattern.size() - 1; i++) {
+        if (check_column_symetry(pattern, i)) {
+          v_pos = i;
+          break;
         }
       }
-      // std::cout << '\n';
-      if (positions.size() == 1) {
-        for (auto a : pattern) {
-          std::cout << a << '\n';
-        }
-        total_sum += act_pos;
-        std::cout << "Lines Mirror on index " << act_pos << "\n\n";
-      } else {
-
-        //  for (auto a : pattern) {
-        //  std::cout << a << '\n';
-        // }
-        positions.clear();
-        up_side_equals_down_side(pattern, pos, act_pos, positions);
-
-        if (positions.size() == 1) {
-          for (auto a : pattern) {
-            std::cout << a << '\n';
-          }
-          total_sum += 100 * act_pos;
-          std::cout << "Lines Mirror Verticaly on index " << act_pos << "\n\n";
+      std::vector<std::string> T = transpose(pattern);
+      for (int i = 0; i < T.size() - 1; i++) {
+        if (check_column_symetry(T, i)) {
+          h_pos = i;
+          break;
         }
       }
-      positions.clear();
+      total_sum += h_pos + 1 + (v_pos + 1) * 100;
       pattern.clear();
-    } else {
+    }
+
+    else {
       pattern.push_back(input_line);
     }
   }
-  std::cout << total_sum << "\n";
+  std::cout << total_sum << '\n';
   return 0;
 }
