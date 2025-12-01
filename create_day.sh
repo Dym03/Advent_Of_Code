02242682669a42a96e62c77c142aa5c5c5b3d74b
Dym03 <hdymacek@gmail.com>
2025-12-01 10:36:54 +0100

    Updated create day for rust project

--- create_day.sh
+++ create_day.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
 if [ -z $1 ]; then
-	echo "enter year and day"
-	exit 1
+  echo "enter year and day"
+  exit 1
 elif [ -z $2 ]; then
-	echo "enter day"
-	exit 1
+  echo "enter day"
+  exit 1
 fi
 
 if [ ! -d "Advent_$1/" ]; then
-	mkdir Advent_$1
+  mkdir Advent_$1
 elif [ -d "Advent_$1/Day_$2" ]; then
-	echo "Directory exists ... so go there you dummy"
-	exit 1
+  echo "Directory exists ... so go there you dummy"
+  exit 1
 fi
 
 cd Advent_$1
-mkdir Day_$2
-cd Day_$2
-cp ../../Utils/template.cpp main.cpp
+if [ "$3" == "rust" ]; then
+  cargo new Day_$2
+  cd Day_$2
+else
+  mkdir Day_$2
+  cd Day_$2
+  cp ../../Utils/template.cpp main.cpp
+fi
+
 touch test.txt
 python ../../Utils/get_inputs.py $1 $2
